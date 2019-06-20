455535a9f7ee3813ae026b5c4cba3a935f26abf0
hema555 <51548468+hema555@users.noreply.github.com>
2019-06-20 09:30:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -301,6 +301,21 @@ client.on("message", message => {
   message.delete();
   };
   });
+var prefix = "!n";
+
+  client.on("message", message => {
+  
+              if (message.content.startsWith(prefix + "bc")) {
+                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+    let args = message.content.split(" ").slice(1);
+    var argresult = args.join(' '); 
+    message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+   m.send(`${argresult}\n ${m}`);
+  })
+   message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` :mailbox:  عدد المستلمين `); 
+   message.delete(); 
+  };     
+  });
  client.on('message', message => {
     if (message.content.startsWith(prefix + "bot")) {
     message.channel.send({
